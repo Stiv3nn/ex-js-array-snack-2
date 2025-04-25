97d5af6d8350aa4cdbcfdc776d84984b94f75b71
Stiven hoxha <stivenhoxha007@gmail.com>
2025-04-25 16:18:49 +0200

    snack 2

--- script.js
+++ script.js
@@ -50,14 +50,42 @@ const books = [
 // Crea una funzione che somma due numeri.
 
 // Crea un array (longBooks) con i libri che hanno più di 300 pagine;
-const longBooks = books.filter(b => b.pages > 300);
-console.log(longBooks);
+// const longBooks = books.filter(b => b.pages > 300);
+// console.log(longBooks);
 
 // Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
-const longBooksTitles = books.filter(b => b.pages > 300).map(b => b.title);
+// const longBooksTitles = books.filter(b => b.pages > 300).map(b => b.title);
 
-console.log(longBooksTitles);
+// console.log(longBooksTitles);
 
 // Stampa in console ogni titolo nella console.
 
-longBooksTitles.forEach(t => console.log(t));
+// longBooksTitles.forEach(t => console.log(t));
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// Snack 2 - Il primo libro scontato
+
+// Creare un array (availableBooks) che contiene tutti i libri disponibili.
+const availableBooks = books.filter(b => b.available === true);
+console.log(availableBooks);
+
+// Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+const discountedBooks = availableBooks.map(book => {
+    const price = parseFloat(book.price.replace('€', '')); // parseFloat NUMERI CON LA VIRGOLA
+    const discountedPrice = (price * .8).toFixed(2); // toFixed TOGLIE I NUMERI DOPO LA VIRGOLA CHE NON SERVEONO
+    return {
+        ...book,
+        price: `${discountedPrice}€`
+    }
+})
+console.log(discountedBooks);
+
+
+// Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+const fullPricedBook = discountedBooks.find(book => {
+    const price = parseFloat(book.price.replace('€', ''));
+    // return price % 1 === 0;
+    return Number.isInteger(price); // HA LA STESSA FUNZIONE CON LA RIGA 88....CI RITORNA UN BOOLEANO TRUE O FALSE SE IL NUMERO E' INTERO
+});
+console.log(fullPricedBook);
