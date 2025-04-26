ae7346200caabf994218c71420c74a0e9525374f
Stiven hoxha <stivenhoxha007@gmail.com>
2025-04-26 10:59:27 +0200

    bonus snack 7

--- script.js
+++ script.js
@@ -168,20 +168,40 @@ const books = [
 
 // Snack 6 (Bonus) - Ordina i libri
 
-// Crea una variabile booleana (areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
-const areThereAvailableBooks = books.some(b => b.available);
-console.log(areThereAvailableBooks);
+// // Crea una variabile booleana (areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
+// const areThereAvailableBooks = books.some(b => b.available);
+// console.log(areThereAvailableBooks);
 
-// Crea un array (booksByPrice) con gli elementi di books ordinati in base al prezzo (crescente).
-const booksByPrice = [...books].sort((a, b) =>{
+// // Crea un array (booksByPrice) con gli elementi di books ordinati in base al prezzo (crescente).
+// const booksByPrice = [...books].sort((a, b) =>{
 
-    const priceA = parseFloat(a.price.replace('€', ''));
-    const priceB = parseFloat(b.price.replace('€', ''));
-    return priceA - priceB;
+//     const priceA = parseFloat(a.price.replace('€', ''));
+//     const priceB = parseFloat(b.price.replace('€', ''));
+//     return priceA - priceB;
 
-});
-// console.log(...books, booksByPrice);
+// });
+// // console.log(...books, booksByPrice);
 
-// Ordina l’array booksByPricein base alla disponibilità (prima quelli disponibili), senza creare un nuovo array.
-booksByPrice.sort((a, b) => a.available === b.available ? 0 : a.available ? -1 : 1 );
-console.log(booksByPrice);
+// // Ordina l’array booksByPricein base alla disponibilità (prima quelli disponibili), senza creare un nuovo array.
+// booksByPrice.sort((a, b) => a.available === b.available ? 0 : a.available ? -1 : 1 );
+// console.log(booksByPrice);
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// Snack 7 (Bonus) - Analizza i tag
+
+// Usa reduce per creare un oggetto (tagCounts) che conta quante volte ogni tag viene usato tra i libri.
+const tagCounts = books.reduce((accumulator, book) => {
+
+    book.tags.forEach(tag => {
+        if(!accumulator[tag]){
+            accumulator[tag] = 1;
+        } else{
+            accumulator[tag] ++;
+        }
+    });
+    return accumulator;
+}, {});
+
+console.log(tagCounts);
+
